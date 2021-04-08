8bb3c333072fd020dc4f57208d442e52006c0f83
qwertyuio <mmm606@inbox.ru>
2021-04-08 09:14:50 +0300

    Getting started

---
+++ AiBigStand/Game1.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using AutoNeuralNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.IO;
+using System.Threading;
+//using System.Windows.Forms;
+
+namespace AiBigStand
+{
+    public class Game1 : Game
+    {
+        Keys lastKey = Keys.Down;
+        StreamWriter streamWriter = new StreamWriter(@"C:\Users\Mikhail\source\repos\AiBigStand\AiBigStand\traindata.txt", true);
+        double[] answers;
+        private Timer neuralUpdate;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D rectangleBlock;
+        NeuralCore net;
+        Rectangle r;
+        private const int cellSize = 15;
+        private const int cellCount = 25;
+        private double[,] matrix;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = cellCount * cellSize + 2 * cellSize + cellSize;
+            _graphics.PreferredBackBufferHeight = cellCount * cellSize;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            answers = new double[10];
+            //net = new NeuralCore(@"C:\Users\Mikhail\source\repos\AINumberTrain\AINumberTrain\links.txt");
+            //neuralUpdate = new Timer();
+            //neuralUpdate.Tick += NeuralUpdate_Tick;
+            //neuralUpdate.Interval = 250;
+            //neuralUpdate.Start();
+
+            base.Initialize();
+        }
+
+        private void NeuralUpdate_Tick(object sender, EventArgs e)
+        {
+            var localMatrix = new List<double>();
+            foreach (var item in matrix)
+            {
+                localMatrix.Add(item);
+            }
+            answers = net.RunNet(localMatrix.ToArray());
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // TODO: use this.Content to load your game content here
+            matrix = new double[cellCount, cellCount];
+            rectangleBlock = new Texture2D(GraphicsDevice, 1, 1);
+            r = new Rectangle(new Point(0, 0), new Point(cellSize, cellSize));
+        }
+
+        private void AddToMatrix(Vector2 pos)
+        {
+            int x = (int)pos.X;
+            int y = (int)pos.Y;
+            matrix[x, y] = 1;
+            if (x + 1 < cellCount)
+            {
+                if (matrix[x + 1, y] == 0)
+                    matrix[x + 1, y] = 0.5;
+            }
+            if (x - 1 > -1)
+            {
+                if (matrix[x - 1, y] == 0)
+                    matrix[x - 1, y] = 0.5;
+            }
+            if (y + 1 < cellCount)
+            {
+                if (matrix[x, y + 1] == 0)
+                    matrix[x, y + 1] = 0.5;
+            }
+            if (y - 1 > -1)
+            {
+                if (matrix[x, y - 1] == 0)
+                    matrix[x, y - 1] = 0.5;
+            }
+        }
+
+        private void SaveImage(string a, StreamWriter stream)
+        {
+            string dataLine = string.Empty;
+            List<string> parts = new List<string>();
+            foreach (var item in matrix)
+            {
+                parts.Add(item.ToString());
+            }
+            dataLine = string.Join("|", parts);
+            stream.WriteLine(dataLine + $" {a}");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            MouseState state = Mouse.GetState();
+            Vector2 selectedCell = new Vector2(state.X / cellSize, state.Y / cellSize);
+            if (selectedCell.X < cellCount && selectedCell.Y < cellCount && selectedCell.Y > -1 && selectedCell.X > -1)
+            {
+                if (state.LeftButton == ButtonState.Pressed) AddToMatrix(selectedCell);
+                else if (state.RightButton == ButtonState.Pressed) matrix[(int)selectedCell.X, (int)selectedCell.Y] = 0;
+                else if (Keyboard.GetState().IsKeyDown(Keys.Q)) matrix = new double[cellCount, cellCount];
+            }
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (!keyboardState.GetPressedKeys().Contains(lastKey) && keyboardState.IsKeyDown(Keys.LeftControl) && keyboardState.GetPressedKeys().Length == 3)
+            {
+                if (lastKey == Keys.Down)
+                    lastKey = Keys.Up;
+                else
+                    lastKey = Keys.Down;
+                string answer = string.Empty;
+                switch (keyboardState.GetPressedKeys()[1])
+                {
+                    case Keys.NumPad0:
+                        answer = "0000000001";
+                        break;
+                    case Keys.NumPad1:
+                        answer = "1000000000";
+                        break;
+                    case Keys.NumPad2:
+                        answer = "0100000000";
+                        break;
+                    case Keys.NumPad3:
+                        answer = "0010000000";
+                        break;
+                    case Keys.NumPad4:
+                        answer = "0001000000";
+                        break;
+                    case Keys.NumPad5:
+                        answer = "0000100000";
+                        break;
+                    case Keys.NumPad6:
+                        answer = "0000010000";
+                        break;
+                    case Keys.NumPad7:
+                        answer = "0000001000";
+                        break;
+                    case Keys.NumPad8:
+                        answer = "0000000100";
+                        break;
+                    case Keys.NumPad9:
+                        answer = "0000000010";
+                        break;
+                    default:
+                        answer = "non";
+                        break;
+                }
+                try
+                {
+                    if (answer != "non")
+                    {
+                        SaveImage(answer, streamWriter);
+                        matrix = new double[cellCount, cellCount];
+                    }
+                }
+                finally
+                {
+
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        public void DrawRect(Vector2 pos, Color color)
+        {
+            Color xnaColorBorder = new Color(128, 128, 128); // default color gray
+            rectangleBlock.SetData(new[] { xnaColorBorder });
+            r.Location = new Point((int)pos.X, (int)pos.Y);
+            r.Size = new Point(cellSize, cellSize);
+            _spriteBatch.Draw(rectangleBlock, r, color);
+
+        }
+
+        public void DrawRect(Vector2 size, Vector2 pos, Color color)
+        {
+
+            Color xnaColorBorder = new Color(128, 128, 128); // default color gray
+            rectangleBlock.SetData(new[] { xnaColorBorder });
+            r.Location = new Point((int)pos.X, (int)pos.Y);
+            r.Size = new Point((int)size.X, (int)size.Y);
+            _spriteBatch.Draw(rectangleBlock, r, color);
+
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    DrawRect(new Vector2(cellSize * i, cellSize * j), new Color((int)(255 * matrix[i, j]), (int)(255 * matrix[i, j]), (int)(255 * matrix[i, j])));
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                var k = answers[i];
+                if (k != answers.Max()) k = 0;
+                DrawRect(new Vector2(cellSize * cellCount + cellSize, cellSize * i + 10 * i), new Color((float)(255 * k), (float)(255 * k), (float)(225 * k)));
+            }
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        protected override void UnloadContent()
+        {
+            streamWriter.Close();         
+            streamWriter.Dispose();
+            base.UnloadContent();
+        }
+    }
+}
---
+++ AiBigStand/NeuralCore.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace AutoNeuralNet
+{
+    class NeuralCore
+    {
+        double[][,] links;
+
+        public void SaveLinks()
+        {
+            StreamWriter stream = new StreamWriter(@"C:\Users\Mikhail\source\repos\ANNGit\AutoNeuralNet\AutoNeuralNet\links.txt");
+            string tlinks = JsonConvert.SerializeObject(links, Formatting.Indented);
+            stream.WriteLine(tlinks);
+            stream.Close();
+            stream.Dispose();
+        }
+
+        private double F(double x) => (2 / (1 + Math.Exp(-x))) - 1;
+
+        private double Df(double x) => 0.5 * (1 + F(x)) * (1 - F(x));
+
+        private void GetRandomMatrix(ref double[,] matrix)
+        {
+            Random random = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.NextDouble();
+                }
+            }
+        }
+
+        private void SaveToLog(StreamWriter writer, object[] values)
+        {
+            foreach (var item in values)
+            {
+                writer.WriteLine(item.ToString());
+            }
+        }
+
+        private double[] Dot(double[,] matrix, double[] values)
+        {
+            double[] final = new double[matrix.GetLength(1)];
+            for (int i = 0; i < values.Length; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    final[j] += values[i] * matrix[i, j];
+                }
+            }
+            return final.Select(i => 2 / (1 + Math.Exp(-i)) - 1).ToArray();
+        }
+
+        private double[] TrainigDot(double[,] matrix, double[] values, out double[] nonActivatedOut)
+        {
+            double[] final = new double[matrix.GetLength(1)];
+            for (int i = 0; i < values.Length; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    final[j] += values[i] * matrix[i, j];
+                }
+            }
+            nonActivatedOut = final;
+            return final.Select(i => (2 / (1 + Math.Exp(-i))) - 1).ToArray();
+        }
+
+        private double[] TrainRun(double[] values, out double[][] localInputs, out double[][] localOuts)
+        {
+            localInputs = new double[links.Length][];
+            localOuts = new double[links.Length + 1][];
+            double[] tmpResults = values;
+            localOuts[0] = values;
+            for (int i = 0; i < links.Length; i++)
+            {
+                tmpResults = TrainigDot(links[i], tmpResults, out localInputs[i]);
+                localOuts[i + 1] = tmpResults;
+            }
+            return tmpResults;
+        }
+
+        public NeuralCore(int[] config)
+        {
+            links = new double[config.Length - 1][,];
+            for (int i = 0; i < config.Length - 1; i++)
+            {
+                links[i] = new double[config[i], config[i + 1]];
+                GetRandomMatrix(ref links[i]);
+            }
+        }
+
+        public NeuralCore(string linksPath)
+        {
+            links = JsonConvert.DeserializeObject<double[][,]>(new StreamReader(linksPath).ReadToEnd());
+        }
+
+        public void SetMatrix(double[,] matrix, int layer) { this.links[layer] = matrix; }
+
+        public void SetAllMatrixes(double[][,] m) { links = m; }
+
+        public double[] RunNet(double[] values)
+        {
+            if (values.Length != links[0].GetLength(0))
+            {
+                throw new ArgumentOutOfRangeException($"Your inputs length is [{values.Length}], but first matrix rank is [{links[0].GetLength(0)}:{links[0].GetLength(1)}] ");
+
+            }
+            else
+            {
+                double[] tmpResults = values;
+                for (int i = 0; i < links.Length; i++)
+                {
+                    tmpResults = Dot(links[i], tmpResults);
+                }
+                return tmpResults;
+            }
+
+        }
+          
+        
+
+        public void StartTraining(int iterations, double[][] tests, double[][] correctOutputs, double lmd = 0.001)
+        {
+            StreamWriter stream = new StreamWriter("C:\\Users\\Mikhail\\Desktop\\Logs\\snn2.log.txt");
+            for (int N = 0; N < iterations; N++)
+            {
+                var deltaToSave = 0.0;
+                var k = N % tests.Length;
+                double[] currentIn = tests[k];
+                double[] trueOut = correctOutputs[k];
+                double[] currentOut = TrainRun(currentIn, out double[][] localInputs, out double[][] localOuts);
+                double[] gradients = new double[localOuts.Last().Length];
+                var e = 0.0;
+                for (int i = links.Length - 1; i >= 0; i--)
+                {
+
+                    if (i == links.Length - 1)
+                    {
+                        for (int j = 0; j < gradients.Length; j++)
+                        {
+                            double error = currentOut[j] - trueOut[j];
+                            gradients[j] = error * Df(currentOut[j]);
+                            e = error;
+                        }
+
+                        deltaToSave = gradients[0];
+                    }
+                    else
+                    {
+                        double[] nextGradients = new double[localOuts[i + 1].Length];
+                        for (int j = 0; j < nextGradients.Length; j++)
+                        {
+                            for (int a = 0; a < links[i + 1].GetLength(1); a++)
+                            {
+
+                                nextGradients[j] += gradients[a];
+                                nextGradients[j] *= links[i + 1][j, a];
+                                nextGradients[j] *= Df(localOuts[i + 1][j]);
+                            }
+
+                        }
+                        gradients = nextGradients;
+                    }
+                    for (int x = 0; x < links[i].GetLength(0); x++)
+                    {
+                        for (int y = 0; y < links[i].GetLength(1); y++)
+                        {
+                            var d = lmd * gradients[y] * localOuts[i][x];
+                            //Console.WriteLine($" Delta {d}, X {x}, Y {y}, Layer {i}");
+                            links[i][x, y] -= d;
+                        }
+                    }
+
+                }
+                SaveToLog(stream, new object[] { links[0][0, 0], e });
+            }
+            stream.Close();
+            stream.Dispose();
+        }
+    }
+}
